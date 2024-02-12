75e1d51760b10f89e36f95caec0d3c18d4d69961
Candidat-005 <manu.diogo@yahoo.fr>
2024-02-12 14:43:29 +0100

    saison2

--- exercice10.js
+++ exercice10.js
@@ -1,4 +1,4 @@
-// Exercice 10: Convertir une chaîne de caractères en camelCase
+// Exercice 10: Convertir une chaîne de caractères en camelCas
 function convertToCamelCase(chaine) {
     return chaine.replace(/[-_]\w/g, match => match.charAt(1).toUpperCase());
 }
